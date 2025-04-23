bd68937c82ba6c4de46bd66eb802e142c20ff164
Rei <60601546+Lukas-Bouhlel@users.noreply.github.com>
2025-04-23 16:30:57 +0200

    bug fixed eslint

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,6 +1,6 @@
 function isPalindrome(word) {
-  if (typeof word !== "string") return false;
-  const clean = word.toLowerCase().replace(/[^a-z0-9]/g, "");
-  return clean === clean.split("").reverse().join("");
+  if (typeof word !== 'string') return false;
+  const clean = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return clean === clean.split('').reverse().join('');
 }
 module.exports = isPalindrome;
